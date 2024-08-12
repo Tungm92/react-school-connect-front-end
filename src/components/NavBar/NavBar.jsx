2d9e58b65beac82dd131bb1b80bb30461aefb0f5
Timothy Weng <timothy880606@gmail.com>
2024-08-12 10:45:15 -0500

    create student form

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -9,6 +9,8 @@ const NavBar = ({ user, handleSignout }) => {
           <nav>
             <ul>
               <li><Link to="/">Home</Link></li>
+              <li><Link to="/">All Logs</Link></li>
+              <li><Link to="/">Students</Link></li>
               <li><Link to="" onClick={handleSignout}>Sign Out</Link></li>
             </ul>
           </nav>
