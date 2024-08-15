85592508f1af7d32e5a00488138f30f15e467acd
Timothy Weng <timothy880606@gmail.com>
2024-08-15 09:54:07 -0500

    finish create add delete for logs

--- src/components/StudentLogs/LogDetails/LogDetails.jsx
+++ src/components/StudentLogs/LogDetails/LogDetails.jsx
@@ -1,12 +1,13 @@
 import {useState, useEffect} from 'react';
-import { useNavigate, useParams } from 'react-router-dom';
+import {useParams, Link, useNavigate } from 'react-router-dom';
 
-    const LogDetails = ({getLogById}) => {
+    const LogDetails = ({getLogById, deleteLog}) => {
  
         const {logId} = useParams() 
-    
+        const navigate = useNavigate();
         const [logData, setLogData] = useState(null)
     
+
         useEffect(() => {
             const fetchLogData = async () => {
                 const data = await getLogById(logId);  
@@ -19,14 +20,24 @@ import { useNavigate, useParams } from 'react-router-dom';
             return <p>Loading...</p>; 
         }
 
+        const handleDelete = async () => {
+            try {
+                await deleteLog(logData._id);
+                navigate('/mylogs');  
+            } catch (error) {
+                console.error('Failed to delete log:', error);
+                alert('An error occurred while deleting the log.');
+            }
+        };
+
     return(
         <>
             <h1>{logData.studentId.lastName}, {logData.studentId.firstName} Log</h1>
             <br />
             <p>Title: {logData.purpose}</p>
             <p>Content: {logData.notes}</p>
-            <button>Edit</button>
-            <button>Delete</button>
+            <Link to={`/students/${logData.studentId._id}/edit/logs/${logData._id}`}>Edit</Link>
+            <button onClick={handleDelete}>Delete</button>
         </>
     )
 }
